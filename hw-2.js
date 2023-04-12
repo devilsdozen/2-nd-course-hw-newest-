0c96a0d96689ac667b1af7abfa82d0ad862bfad0
devilsdozen <devilsdozen@mail.ru>
2023-04-13 00:05:16 +0500

    created hw of js

--- hw-2.js
+++ hw-2.js
@@ -1,2 +1,59 @@
-let a = 10;
-confirm(a);
+// // Задание 1
+// let a = 10;
+// alert(a);
+
+// // Задание 2
+// const createIphone = 2007;
+// alert(createIphone);
+
+// // Задание 3
+// const nameCreator = 'Brendon Eich';
+// alert(nameCreator);
+
+// // Задание 4
+// let c = 10;
+// let d = 2;
+// alert(c + d);
+// alert(c - d);
+// alert(c * d);
+// alert(c / d);
+
+// // Задание 5
+// let result = 2 ** 5;
+// alert(result);
+
+// // Задание 6
+// let a = 9;
+// let b = 2;
+// let result2 = a % b;
+// alert(result2);
+
+// // Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+// //Задание 8
+// let age = Number(prompt("Сколько вам лет?"));
+// alert(age);
+
+// // Задание 9.0-9.4
+// const user = {
+//     name: "Sam",
+//     age: 31,
+//     isAdmin: true,
+// }
+// user.age = 32;
+// user['city of residence'] = "Chelyabinsk";
+// delete user ['city of residence'];
+// let info = prompt("Что вы хотите узнать о пользователе?", "for example, city of residence");
+// alert(user[info]);
+
+// // Задание 10
+// let name = prompt('Как вас зовут?');
+// alert(`Привет, ${name}!`);
