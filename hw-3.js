7eb26de18fc221f57f143ca0be94a5dfd6825e00
devilsdozen <devilsdozen@mail.ru>
2023-04-18 17:38:27 +0500

    сделал задание 1

---
+++ hw-3.js
@@ -0,0 +1,14 @@
+// // Задание 1
+// // вариант 1
+// let password = 'пароль';
+// let check = String(prompt('Введите пароль'));
+// if (check === password) {
+//     alert('Пароль введен верно');
+// } else {
+//     alert('Пароль введен неправильно');
+// }
+// // вариант 2
+// let password = 'пароль';
+// let check = String(prompt('Введите пароль'));
+// let message = (check === password) ? 'Пароль введен верно' : 'Пароль введен неправильно';
+// alert(message);
