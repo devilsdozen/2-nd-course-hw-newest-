2c1b65a67ff0a71b7d6ca9a6052adc0a5656fd6e
devilsdozen <devilsdozen@mail.ru>
2023-04-27 00:52:50 +0500

    новая дз 6

---
+++ cw-6.js
@@ -0,0 +1,54 @@
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el) => {
+// 	console.log(el);
+// });
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// for (let i = 0; i < numbs.length; i++) {
+// 	console.log(numbs[i]);
+// }
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el, index) => {
+// 	console.log(`${index}: ${el}`);
+// });
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el, index, arr) => {
+// 	console.log(`${index}: ${el}, массив: ${arr}`);
+// });
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el) => {
+	// if (el == 5) break;  // не сработает break
+// 	console.log(el);
+// });
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// for (let i = 0; i < numbs.length; i++) {
+// 	if (numbs[i] == 5) break;
+// 	console.log(numbs[i]);
+// }
+
+let word = 'abcdef';
+word = word.split('');
+console.log(word);
+console.log(word.length);
+word.forEach((el, index = 1) => {
+    console.log(`${index}: ${el}`);
+});
+    
+// function searchLetter() {
+//     if () {
+        
+//     } else {
+        
+//     }
+    
+// }
